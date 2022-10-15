2894f307f2a2c198b1e882abc574ca9df7888dbd
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-15 14:00:11 -0500

    Try to fix image render on gh-pages

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "images/wizard.png",
+        avatar: "./images/wizard.png",
         health: 10,
         diceCount: 3,
     },
     monster: {
         name: "Orc",
-        avatar: "images/orc.png",
+        avatar: "./images/orc.png",
         health: 10,
         diceCount: 1,
     },
