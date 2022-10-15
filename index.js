9b03833fb5e83e3e6f71012960af3d7255b27a90
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-15 13:22:25 -0500

    Add getDiceHtml method inside constructor

--- index.js
+++ index.js
@@ -20,29 +20,31 @@ function getDiceRollArray(diceCount) {
     })
 }
 
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function(num){ 
-        return  `<div class="dice">${num}</div>`
-    }).join('')
-}
-
-function Character (data) {
+function Character(data) {
     Object.assign(this, data)
-    this.getCharacterHtml = function () {
-        const {elementId,name, avatar, health,  diceCount} = this;
 
-        let diceHtml = getDiceHtml(diceCount)
+    this.getDiceHtml = function (diceCount) {
+        return getDiceRollArray(diceCount).map(function (num) {
+            return `<div class="dice">${num}</div>`
+        }).join('')
+    }
+
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount, getDiceHtml} = this;
+        let diceHtml = getDiceHtml(diceCount);
 
         document.getElementById(elementId).innerHTML = `
-        <div class="character-card">
-            <h4 class="name">${name}</h4>
-            <img class="avatar" src="${avatar}"/>
-            <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container">${diceHtml}</div>
-        </div>
-        `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`;
     }
 }
+
 
 
 const wizard = new Character(hero)
