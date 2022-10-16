2fde2db784ede9cdc3f7503c9dfc60d3b05baaa0
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-16 17:05:58 -0500

    Add data for new monsters

--- data.js
+++ data.js
@@ -10,10 +10,24 @@ const characterData = {
     monster: {
         name: "Orc",
         avatar: "./images/orc.png",
-        health:40,
+        health: 30,
         diceCount: 1,
         currentDiceScore: [],
         dead: false,
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "./images/goblin.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "./images/demon.png",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
     },
 }
 
