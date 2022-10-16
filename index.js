ff205bad6f490da61caad7e80713ead5b509a62b
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-16 17:18:44 -0500

    Fix broken app to dislpay monster again

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import characterData from "./data.js"
 import  Character  from "./Character.js"
 
+let monstersArray = ["orc", "goblin", "demon"];
 
-const wizard = new Character(characterData.hero)
-const orc = new Character(characterData.monster)
+function getNewMonster () {
+    const nextMonsterData = characterData[monstersArray.shift()]
+
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
 
 
 function render () {
     document.getElementById('hero').innerHTML =  wizard.getCharacterHtml()
-    document.getElementById('monster').innerHTML = orc.getCharacterHtml()   
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()   
 }
 
 function endGame(){
-    let endMessage = wizard.health === 0 && orc.health === 0 ? "No victors - all creatures are dead" : wizard.health > 0 ? "The Wizard Wins" : "The Orc is Victorious";
+    let endMessage = wizard.health === 0 && monster.health === 0 ? "No victors - all creatures are dead" : wizard.health > 0 ? "The Wizard Wins" : `The ${monster.name} is Victorious`;
 
-    const endEmoji = wizard.health === 0 && orc.health === 0 ? "☠️" : wizard.health > 0 ? "🔮" : "☠️";
+    const endEmoji = wizard.health === 0 && monster.health === 0 ? "☠️" : wizard.health > 0 ? "🔮" : "☠️";
     console.log(endEmoji)
 
     document.body.innerHTML = `
@@ -26,15 +30,19 @@ function endGame(){
 }
 function attack () {
     wizard.getDiceHtml();
-    orc.getDiceHtml();
-    wizard.takeDamage(orc.currentDiceScore);
-    orc.takeDamage(wizard.currentDiceScore);
-    if (wizard.dead || orc.dead ) {
+    monster.getDiceHtml();
+    wizard.takeDamage(monster.currentDiceScore);
+    monster.takeDamage(wizard.currentDiceScore);
+    if (wizard.dead || monster.dead ) {
         endGame()
     }
     render()
 }
 
+
 document.getElementById('attack-button').addEventListener('click', attack)
+
+const wizard = new Character(characterData.hero)
+const monster = getNewMonster()
 
 render()
